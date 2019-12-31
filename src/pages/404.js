2394c4e64827319313283ee664cfcd956ab8dd50
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-31 12:52:00 +0300

    header module styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,12 +9,12 @@ export default () => (
   <Layout>
     <div className={styles.content}>
       <h1 className={styles.header}>Page not found</h1>
-      <div>
-        <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
-      </div>
       <p className={styles.errorMessage}>
         The page you  are looking for does not exist!
       </p>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
     </div>
   </Layout>
 );
