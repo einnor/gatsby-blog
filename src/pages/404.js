805364d807d153c8c7a7230bff83476e1c36a580
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-31 12:49:30 +0300

    Wrap all the pages in the new layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Layout from '../components/layout';
+
 import styles from './404.module.scss';
 
 export default () => (
-  <div className={styles.content}>
-    <h1 className={styles.header}>Page not found</h1>
-    <div>
-      <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
+  <Layout>
+    <div className={styles.content}>
+      <h1 className={styles.header}>Page not found</h1>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
+      </div>
+      <p className={styles.errorMessage}>
+        The page you  are looking for does not exist!
+      </p>
     </div>
-    <p className={styles.errorMessage}>
-      The page you  are looking for does not exist!
-    </p>
-  </div>
+  </Layout>
 );
