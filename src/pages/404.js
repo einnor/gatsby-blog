6734fab49a116c888ff2e62be6a2c11a80681ce9
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-31 12:18:52 +0300

    Linking between pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default () => (
   <div>
     <h1>Page not found</h1>
+    <div>
+      <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
+    </div>
     <p>
       The page you  are looking for does not exist!
     </p>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default () => (
   <div>
     <h1>This is the about page</h1>
+    <div>
+      <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
+    </div>
     <p>
       Lorem ipsum dolorsit amet
     </p>
