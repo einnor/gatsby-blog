19d297b54f459bfc6c64ca62fadaab236436e128
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-31 13:07:37 +0300

    Add the header and footer to the layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,18 @@
 
 import React from 'react';
+
+import Header from './header.js'
+import Footer from './footer.js'
 import styles from './layout.module.scss';
 
 export default ({ children }) => (
    <div className={styles.container}>
-      <div className={styles.content}>
-        {children}
-      </div>
+    <Header />
+    <div className={styles.content}>
+      {children}
+    </div>
+    <Footer>
+      My Gatsby Blog 2020
+    </Footer>
    </div>
 )
