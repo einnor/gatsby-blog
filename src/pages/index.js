eb09508a4618dbdfd5a45278c2139ea95785c443
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-31 16:03:18 +0300

    Querying site metadata in a page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 import Layout from '../components/layout';
 import Title from '../components/title';
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <Title text="Welcome" />
+    <Title text={data.site.siteMetadata.title} />
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida, magna vitae pharetra sagittis, libero orci vehicula nulla, nec laoreet diam massa et tortor. Sed tincidunt est sit amet velit varius mollis. Curabitur dapibus et nibh a fringilla. Morbi nec neque viverra, feugiat nunc sed, facilisis est. In at congue arcu. Donec congue ullamcorper dapibus. Morbi at mauris pretium, posuere urna ac, consequat quam. Nullam feugiat sem dolor. Proin fermentum rhoncus purus, ut lobortis ante tristique sed. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce imperdiet diam ante, id malesuada ipsum commodo id. Pellentesque molestie tempus mollis. Proin ex quam, convallis posuere tortor vitae, euismod lobortis elit. Mauris eget sapien sit amet diam ullamcorper sodales eu eu ante. Interdum et malesuada fames ac ante ipsum primis in faucibus.
     </p>
   </Layout>
 );
+
+export const query = graphql `query {
+  site {
+    siteMetadata {
+      title
+    }
+  }
+}`;
