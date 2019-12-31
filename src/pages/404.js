fd737d1e6bece8b7dd25081212dc9a051c2bbdc9
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-31 12:28:16 +0300

    Applying styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import styles from './404.module.css';
+
 export default () => (
   <div>
-    <h1>Page not found</h1>
+    <h1 className={styles.header}>Page not found</h1>
     <div>
       <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
     </div>
-    <p>
+    <p className={styles.errorMessage}>
       The page you  are looking for does not exist!
     </p>
   </div>
