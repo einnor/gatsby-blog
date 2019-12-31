2db991448591731c56f3d32134a034793e9447c5
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-31 13:13:36 +0300

    Placeholder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import Title from '../components/title';
 
 export default () => (
   <Layout>
-    <h1>This is the index page</h1>
-    <div>
-      <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
-    </div>
+    <Title text="Welcome" />
     <p>
-      Lorem ipsum dolorsit amet
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida, magna vitae pharetra sagittis, libero orci vehicula nulla, nec laoreet diam massa et tortor. Sed tincidunt est sit amet velit varius mollis. Curabitur dapibus et nibh a fringilla. Morbi nec neque viverra, feugiat nunc sed, facilisis est. In at congue arcu. Donec congue ullamcorper dapibus. Morbi at mauris pretium, posuere urna ac, consequat quam. Nullam feugiat sem dolor. Proin fermentum rhoncus purus, ut lobortis ante tristique sed. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce imperdiet diam ante, id malesuada ipsum commodo id. Pellentesque molestie tempus mollis. Proin ex quam, convallis posuere tortor vitae, euismod lobortis elit. Mauris eget sapien sit amet diam ullamcorper sodales eu eu ante. Interdum et malesuada fames ac ante ipsum primis in faucibus.
     </p>
   </Layout>
 );
